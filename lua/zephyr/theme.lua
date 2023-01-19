6aa7769a76c43d72c37789ac4a91cf9cafc19280
Razak <moabdulrazak17@gmail.com>
2023-01-19 00:06:54 +0000

    chore(theme): make CursorLineNr purple

--- lua/zephyr/theme.lua
+++ lua/zephyr/theme.lua
@@ -26,7 +26,7 @@ return {
 	CursorIM = { fg = P.none, bg = P.none, reverse = true },
 	LineNr = { fg = P.comment },
 	qfLineNr = { fg = P.cyan },
-	CursorLineNr = { fg = P.dark5 },
+	CursorLineNr = { fg = P.purple },
 	DiffAdd = { fg = P.black, bg = P.dark_green },
 	DiffChange = { fg = P.black, bg = P.yellow },
 	DiffDelete = { fg = P.black, bg = P.red },
