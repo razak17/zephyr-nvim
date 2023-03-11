978b3aa034b1b6e66e4b2ea842a8224e7ffc1e18
Razak <moabdulrazak17@gmail.com>
2023-03-11 21:31:22 +0000

    chore(theme): remove terminal colors

--- lua/zephyr/init.lua
+++ lua/zephyr/init.lua
@@ -1,24 +1,5 @@
 local P = require("zephyr.palette")
 
-function P.terminal_color()
-	vim.g.terminal_color_0 = P.bg
-	vim.g.terminal_color_1 = P.red
-	vim.g.terminal_color_2 = P.green
-	vim.g.terminal_color_3 = P.yellow
-	vim.g.terminal_color_4 = P.blue
-	vim.g.terminal_color_5 = P.violet
-	vim.g.terminal_color_6 = P.cyan
-	vim.g.terminal_color_7 = P.bg1
-	vim.g.terminal_color_8 = P.brown
-	vim.g.terminal_color_9 = P.red
-	vim.g.terminal_color_10 = P.green
-	vim.g.terminal_color_11 = P.yellow
-	vim.g.terminal_color_12 = P.blue
-	vim.g.terminal_color_13 = P.violet
-	vim.g.terminal_color_14 = P.cyan
-	vim.g.terminal_color_15 = P.fg
-end
-
 local async_load_plugin
 
 local set_hl = function(tbl)
@@ -26,10 +7,6 @@ local set_hl = function(tbl)
 		vim.api.nvim_set_hl(0, group, conf)
 	end
 end
-
-async_load_plugin = vim.loop.new_async(vim.schedule_wrap(function()
-	P.terminal_color()
-end))
 
 function P.colorscheme()
 	local theme = require("zephyr.theme")
